ecf962234a09c38f2a744176a1a19a3d3403b3c1
malmgrus <malmros.mattias@gmail.com>
2025-04-09 13:06:31 +0200

    Started design on header

--- webshop/src/main.jsx
+++ webshop/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Navigation from './header.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Navigation />
   </StrictMode>,
 )
